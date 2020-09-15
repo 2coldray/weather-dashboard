94c002410f5cf6489b19ad8c361d5d66c72ebc1f
Ray Bray <rbray.bray2@gmail.com>
2020-09-15 19:57:50 -0400

    made new divs to add the temp, wind speed, humidity, uv index, and city name to

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ var weekForecast = $("#forecast_div");
 //JS Variables
 var apiKey = "f64cf3ca5c79a43105f048e67f3d1a6a";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Atlanta&units=imperial&appid=" + apiKey;
+//New Divs to append to DOM
+var name = $("<div>");
+var temp = $("<div>");
+var humid = $("<div>");
+var speed = $("<div>");
+var index = $("<div>");
 
 //Function Definitions
 //1st Ajax Call for Current Weather Forecast
