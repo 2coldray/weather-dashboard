63a5d081fd1d31ab22024ebd8bea28de6a3b6934
Ray Bray <rbray.bray2@gmail.com>
2020-09-15 18:42:06 -0400

    Finally got the api call to work

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ var currentForecast = $("#current-forecast");
 var weekForecast = $("#forecast_div");
 
 //JS Variables
-var apiKey = "f64cf3ca5c79a43105f048e67f3d1a6a";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?=Atlanta&apiid=" + apiKey;
+var apiKey = "ea2e8a81e35136522206b76cc7234d3f";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=f64cf3ca5c79a43105f048e67f3d1a6a";
 
 //Function Definitions
 //Practice Ajax Call First
@@ -18,7 +18,7 @@ $.ajax({
     url : queryURL,
     method : "GET"
 }).then(function(response){
-    console.log(response)
+    console.log(response.main.temp)
 });
 
 //Function calls
