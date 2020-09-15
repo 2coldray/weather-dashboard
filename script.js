37fee17f38652da1c27ae102ee3a15fe5cb9785f
Ray Bray <rbray.bray2@gmail.com>
2020-09-15 19:29:12 -0400

    Sucessfully made the first ajax call. was able to get temperature, humidity, name

--- script.js
+++ script.js
@@ -10,20 +10,29 @@ var weekForecast = $("#forecast_div");
 
 //JS Variables
 var apiKey = "f64cf3ca5c79a43105f048e67f3d1a6a";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Atlanta&appid=" + apiKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Atlanta&units=imperial&appid=" + apiKey;
 
 //Function Definitions
-//Practice Ajax Call First
+//1st Ajax Call for Current Weather Forecast
 $.ajax({
     url : queryURL,
     method : "GET"
 }).then(function(response){
-    console.log(response.main.temp)
+    console.log(response.name)
+    currentForecast.text(response.name + " Current Day Forecast"); 
+    console.log(response.main.temp);
+    currentForecast.text("Temperature: " + response.main.temp + " F");
+    console.log(response.main.humidity);
+    currentForecast.text("Humidity: " + response.main.humidity + " %");
+    console.log(response)
 });
+
+//2nd Ajax Call for Current Weather Forecast
+var uvURL = "h"
 
 //Function calls
 
-//Event Listeners
+//Event Listeners 
 searchBtn.on("click", function() {
     console.log("I've Been clicked!");
 });
