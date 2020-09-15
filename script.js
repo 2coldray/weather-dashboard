452732e466654914b1a624d79e0ab48a5fa2fc08
Ray Bray <rbray.bray2@gmail.com>
2020-09-15 09:05:37 -0400

    Starting to dynamically generate html

--- script.js
+++ script.js
@@ -1,2 +1,55 @@
-console.log("Hello World")
+// console.log("Hello World");
 
+// var forecast_div = $("#forecast_div");
+// var searchBtn = $("#search")
+// var cityInput = $("#cityInput")
+
+// var searchHistory = getData();
+
+// console.log(searchHistory);
+
+// for (var i = 0; i < 5; i++) {
+//   buildForecastCard(i);
+// }
+
+// function searchForCity(){
+//     searchBtn.empty();
+//     var city = cityInput.val().trim()
+//     searchHistory.push(city)
+//     storeData(searchHistory)
+//     console.log(city)
+//     $("#resultsHere").text(searchHistory);
+// }
+
+// function buildForecastCard(forecast) {
+//   var card = $("<div>");
+//   card.addClass("col border p-1").text(forecast);
+//   forecast_div.append(card);
+// }
+
+// function storeData(data) {
+//   localStorage.setItem("history", JSON.stringify(data));
+// }
+
+// function getData() {
+//   var searchHistory = JSON.parse(localStorage.getItem("history"));
+
+//   if (searchHistory === null || !Array.isArray(searchHistory)) {
+//     return [];
+//   };
+
+//   return searchHistory;
+// }
+$(document).ready(function() {
+    console.log("Hello");
+
+//DOM Variables
+
+//JS Variables
+
+//Function Definitions
+
+//Function calls
+
+//Event Listeners
+}); 
