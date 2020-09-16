0233eca7ef902dac3e6d7c57d914f244786d1d5f
Ray Bray <rbray.bray2@gmail.com>
2020-09-16 01:32:22 -0400

    i created buttons with user input, and got the 5 day forecast to work properly. I failed to link the buttons to previous data

--- script.js
+++ script.js
@@ -8,46 +8,15 @@ $(document).ready(function () {
   var currentForecast = $("#current-forecast");
   var weekForecast = $("#forecast_div");
 
-  //New Divs to append to DOM
-  var city = [];
-
   //Function calls
-  //Final Ajax Call
-//   //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
-//   var apiKey = "f64cf3ca5c79a43105f048e67f3d1a6a";
-//   var forecastURL =
-//     "https://api.openweathermap.org/data/2.5/forecast?q=Atlanta&units=imperial&appid=f64cf3ca5c79a43105f048e67f3d1a6a";
-
-//   $.ajax({
-//     url: forecastURL,
-//     method: "GET",
-//   }).then(function (response) {
-//     console.log(response);
-
-//     for (var i = 0; i < response.list.length; i = i + 8) {
-//       console.log(response.list[i]);
-//       //Create Element
-//       var card = $("<div>").addClass("col border p- 1");
-//       var results = $("<div>").text(response.list[i]);
-//       var date = $("<div>").text(response.list[i].dt_txt);
-//       var temp = $("<div>").text("Temp: " + response.list[i].main.temp + " F");
-//       var humid = $("<div>").text(
-//         "Humidity: " + response.list[i].main.humidity + " %"
-//       );
-//       //Append to Card and then element
-//       card.append(date);
-//       card.append(temp);
-//       card.append(humid);
-//       weekForecast.append(card);
-//     }
-//   });
 
   //Event Listeners
-  searchBtn.on("click", function () {
+  searchBtn.on("click", function (event) {
     // console.log("I've Been clicked!");
     //Capture User Input
+    event.preventDefault();
     var userInput = $("#cityInput").val().trim();
-    console.log(userInput);
+    // console.log(userInput);
     //JS Variables
     var apiKey = "f64cf3ca5c79a43105f048e67f3d1a6a";
     var queryURL =
@@ -69,19 +38,32 @@ $(document).ready(function () {
       var speed = $("<div>");
       var lat = response.coord.lat;
       var lon = response.coord.lon;
+      var savedCity = [];
+      var cityButtons = $("<button>");
+      currentForecast.empty();
 
       //Add Content
       name.text(response.name + " Forecast " + moment().format("MM-DD-YYYY"));
       temp.text("Temperature: " + response.main.temp + " F");
       humid.text("Humidity: " + response.main.humidity + " %");
       speed.text("Wind Speed: " + response.wind.speed + " MPH");
-      console.log(response);
+      //   console.log(response);
+      //Add click event for button
 
       //Append
       currentForecast.append(name);
       currentForecast.append(temp);
       currentForecast.append(humid);
       currentForecast.append(speed);
+      savedCity.push(userInput);
+      console.log(userInput);
+
+      for (var i = 0; i < savedCity.length; i++) {
+        cityButtons.text(savedCity[i]);
+        pastSearches.append(cityButtons);
+      }
+      //Send to local storage
+      localStorage.setItem(userInput, JSON.stringify(savedCity));
 
       //2nd Ajax Call for Current Weather Forecast
       //http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
@@ -100,10 +82,18 @@ $(document).ready(function () {
       }).then(function (response) {
         //Create Elements
         var index = $("<div>");
+        index.empty();
 
         //Add content
-        console.log(response);
+        // console.log(response);
         index.text("UV Index: " + response.value);
+        if (response.value < 4) {
+          index.addClass("low");
+        } else if (response.value > 4 && response.value < 8) {
+          index.addClass("medium");
+        } else {
+          index.addClass("high");
+        }
 
         //Append to DOM
         currentForecast.append(index);
@@ -111,16 +101,19 @@ $(document).ready(function () {
         //Final Ajax Call
         //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
         var forecastURL =
-          "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=imperial&appid=" + apiKey;
+          "https://api.openweathermap.org/data/2.5/forecast?q=" +
+          userInput +
+          "&units=imperial&appid=" +
+          apiKey;
 
         $.ajax({
           url: forecastURL,
           method: "GET",
         }).then(function (response) {
-          console.log(response);
-
+          //   console.log(response);
+          weekForecast.empty();
           for (var i = 0; i < response.list.length; i = i + 8) {
-            console.log(response.list[i]);
+            // console.log(response.list[i]);
             //Create Element
             var card = $("<div>").addClass("col border p- 1");
             var results = $("<div>").text(response.list[i]);
