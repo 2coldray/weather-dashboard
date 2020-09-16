f01f51a6304a193e1f4275c35b8ba8df08f32dab
Ray Bray <rbray.bray2@gmail.com>
2020-09-15 21:54:02 -0400

    put both ajax calls in my search button event listener

--- script.js
+++ script.js
@@ -1,72 +1,84 @@
-$(document).ready(function() {
-    console.log("I'm working");
+$(document).ready(function () {
+  console.log("I'm working");
 
-//DOM Variables
-var userInput = $("#cityInput");
-var searchBtn = $("#search");
-var pastSearches = $("#resultsHere");
-var currentForecast = $("#current-forecast");
-var weekForecast = $("#forecast_div");
+  //DOM Variables
+  var userInput = $("#cityInput");
+  var searchBtn = $("#search");
+  var pastSearches = $("#resultsHere");
+  var currentForecast = $("#current-forecast");
+  var weekForecast = $("#forecast_div");
 
-//JS Variables
-var apiKey = "f64cf3ca5c79a43105f048e67f3d1a6a";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Atlanta&units=imperial&appid=" + apiKey;
+  //New Divs to append to DOM
+  var city = [];
 
-//New Divs to append to DOM
-var city = [];
+  //Function calls
 
-//Function Definitions
+  //Event Listeners
+  searchBtn.on("click", function () {
+    // console.log("I've Been clicked!");
+    //Capture User Input
+    var userInput = $("#cityInput").val().trim();
+    console.log(userInput);
+    //JS Variables
+    var apiKey = "f64cf3ca5c79a43105f048e67f3d1a6a";
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      userInput +
+      "&units=imperial&appid=" +
+      apiKey;
+    //Function Definitions
 
-//1st Ajax Call for Current Weather Forecast
-$.ajax({
-    url : queryURL,
-    method : "GET"
-}).then(function(response){
-    //Create elements
-    var name = $("<div>");
-    var temp = $("<div>");
-    var humid = $("<div>");
-    var speed = $("<div>");
+    //1st Ajax Call for Current Weather Forecast
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      //Create elements
+      var name = $("<div>");
+      var temp = $("<div>");
+      var humid = $("<div>");
+      var speed = $("<div>");
+      var lat = response.coord.lat;
+      var lon = response.coord.lon;
 
-    //Add Content
-    name.text(response.name + " Forecast"); 
-    temp.text("Temperature: " + response.main.temp + " F");
-    humid.text("Humidity: " + response.main.humidity + " %");
-    speed.text("Wind Speed: " + response.wind.speed + " MPH");
-    console.log(response);
+      //Add Content
+      name.text(response.name + " Forecast");
+      temp.text("Temperature: " + response.main.temp + " F");
+      humid.text("Humidity: " + response.main.humidity + " %");
+      speed.text("Wind Speed: " + response.wind.speed + " MPH");
+      console.log(response);
 
-    //Append
-    currentForecast.append(name);
-    currentForecast.append(temp);
-    currentForecast.append(humid);
-    currentForecast.append(speed);
+      //Append
+      currentForecast.append(name);
+      currentForecast.append(temp);
+      currentForecast.append(humid);
+      currentForecast.append(speed);
+
+      //2nd Ajax Call for Current Weather Forecast
+      //http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
+
+      var uvURL =
+        "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+        apiKey +
+        "&lat=" +
+        lat +
+        "&lon=" +
+        lon;
+
+      $.ajax({
+        url: uvURL,
+        method: "GET",
+      }).then(function (response) {
+        //Create Elements
+        var index = $("<div>");
+
+        //Add content
+        console.log(response);
+        index.text("UV Index " + response.value);
+
+        //Append to DOM
+        currentForecast.append(index);
+      });
+    });
+  });
 });
-
-//2nd Ajax Call for Current Weather Forecast
-//http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
-
-var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=33.75&lon=-84.39";
-
-$.ajax({
-    url : uvURL,
-    method : "GET"
-}).then(function(response){
-    //Create Elements
-    var index = $("<div>");
-
-    //Add content
-    console.log(response);
-    index.text("UV Index " + response.value)
-
-    //Append to DOM
-    currentForecast.append(index);
-});
-
-//Function calls
-
-//Event Listeners 
-searchBtn.on("click", function() {
-    console.log("I've Been clicked!");
-});
-
-}); 
